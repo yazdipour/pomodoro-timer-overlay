a8c799e251d7d359afc22aa363b7d86263aa5289
Shahriar Yazdipour <shahriar.yazdipour@outlook.com>
2020-09-23 15:04:52 +0200

    Add Counter Functionality

--- PomoTimer/MainWindow.xaml.cs
+++ PomoTimer/MainWindow.xaml.cs
@@ -1,38 +1,98 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace PomoTimer
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private int _locationTop = 32;
+        public string BgColor { get; set; } = "#ff0000";
+
+        public int LocationTop
+        {
+            get => _locationTop;
+            set
+            {
+                _locationTop = value;
+                OnPropertyChanged(nameof(LocationTop));
+            }
+        }
+
+        public int LocationLeft { get; set; } = 82;
+        public float OpacityValue { get; set; } = 0.5f;
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        #endregion INotifyPropertyChanged
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
+
         private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
         {
+            LocationTop = 100;
             MessageBox.Show("Hs");
         }
 
+        #region Counter
+        private DispatcherTimer _counter;
+        private string _pausePlayIcon = "";
+        public string PausePlayIcon
+        {
+            get => _pausePlayIcon;
+            set
+            {
+                _pausePlayIcon = value;
+                OnPropertyChanged(nameof(PausePlayIcon));
+            }
+        }
+        private string _timerLabel = "20:00";
+        public string TimerLabel
+        {
+            get => _timerLabel;
+            set
+            {
+                _timerLabel = value;
+                OnPropertyChanged(nameof(TimerLabel));
+            }
+        }
         private void StartStop_OnClick(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("H");
+            if (_counter == null || !_counter.IsEnabled)
+            {
+                PausePlayIcon = "";
+                CreateCounter(1).Start();
+            }
+            else
+            {
+                PausePlayIcon = "";
+                _counter.Stop();
+                _counter.IsEnabled = false;
+            }
         }
+
+        private DispatcherTimer CreateCounter(int min)
+        {
+            var time = TimeSpan.FromMinutes(min);
+            _counter = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate
+            {
+                time = time.Subtract(TimeSpan.FromSeconds(1));
+                if (time.Seconds == 0)
+                    _counter?.Stop();
+                TimerLabel = time.ToString(@"mm\:ss");
+            }, Application.Current.Dispatcher);
+            return _counter;
+        }
+        #endregion
     }
 }
